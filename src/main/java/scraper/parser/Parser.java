5b5d8e45143df13abf29b6bb2c5592c679cef545
Nik <nikitap4.92@gmail.com>
2017-04-02 22:25:58 +0300

    Add parser

--- src/main/java/scraper/parser/Parser.java
+++ src/main/java/scraper/parser/Parser.java
@@ -5,12 +5,8 @@ import scraper.domain.ParseEntity;
 
 import java.util.List;
 
-/**
- * @author Poshivalov Nikita
- * @since 31.03.2017.
- */
 @FunctionalInterface
 interface Parser {
 
-      ParseEntity<? extends Parsable> parse(List<String> strings);
+    ParseEntity<? extends Parsable> parse(List<String> strings);
 }
