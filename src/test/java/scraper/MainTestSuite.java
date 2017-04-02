c51a5760be346121259ee33142fc900bed94e0f7
Nik <nikitap4.92@gmail.com>
2017-04-02 22:26:20 +0300

    Added test suite

---
+++ src/test/java/scraper/MainTestSuite.java
@@ -0,0 +1,30 @@
+package scraper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import scraper.analyzer.impl.CommonAnalyzerTest;
+import scraper.collector.impl.DefaultWebPagesCollectorTest;
+import scraper.collector.impl.SimpleTextCollectorTest;
+import scraper.collector.impl.SimpleWordCollectorTest;
+import scraper.config.ApplicationPropertiesConfigurationTest;
+import scraper.engine.impl.EngineTests;
+import scraper.logger.LoggerFactoryTest;
+import scraper.parser.impl.DefaultConsoleParserTest;
+import scraper.parser.impl.SimpleTextParserTest;
+import scraper.url.URLValidatorTest;
+import scraper.url.impl.DefaultURLFileReaderTest;
+
+/**
+ * @author Poshivalov Nikita
+ * @since 02.04.17.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({URLValidatorTest.class, DefaultURLFileReaderTest.class,
+        DefaultConsoleParserTest.class, SimpleTextParserTest.class,
+        LoggerFactoryTest.class, ApplicationPropertiesConfigurationTest.class,
+        DefaultWebPagesCollectorTest.class, SimpleWordCollectorTest.class,
+        SimpleWordCollectorTest.class, CommonAnalyzerTest.class,
+        SimpleTextCollectorTest.class, EngineTests.class
+})
+public class MainTestSuite {
+}
