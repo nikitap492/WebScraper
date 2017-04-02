ebc17354a8712592c9a57c7ad1c902111ca11048
Nik <nikitap4.92@gmail.com>
2017-04-02 22:15:41 +0300

    Changes in logger factory

--- src/main/java/scraper/logger/LoggerFactory.java
+++ src/main/java/scraper/logger/LoggerFactory.java
@@ -3,6 +3,7 @@ package scraper.logger;
 import scraper.config.ApplicationPropertiesConfiguration;
 import scraper.domain.Property;
 
+import java.io.PrintStream;
 import java.time.LocalDateTime;
 
 /**
@@ -10,9 +11,18 @@ import java.time.LocalDateTime;
  * @since 31.03.2017.
  */
 public class LoggerFactory {
+    private static PrintStream printStream = System.out;
+
+    public static void setPrintStream(PrintStream printStream) {
+        LoggerFactory.printStream = printStream;
+    }
 
     public static Logger obtain(Class<?> clazz){
-        return new SimpleLogger(clazz);
+        return new SimpleLogger(clazz, printStream);
+    }
+
+    public static Logger obtain(Class<?> clazz, PrintStream printStream){
+        return new SimpleLogger(clazz, printStream);
     }
 
     private LoggerFactory() {
@@ -21,29 +31,28 @@ public class LoggerFactory {
     private static class SimpleLogger implements Logger{
 
         private String className;
+        private PrintStream printStream;
 
-        SimpleLogger(Class<?> clazz) {
+        SimpleLogger(Class<?> clazz, PrintStream printStream) {
             this.className = clazz.getName();
+            this.printStream = printStream;
         }
 
         public void error(String msg) {
-            System.err.println(LocalDateTime.now() + " " + className+ " error: " + msg);
+            printStream.println(LocalDateTime.now() + " " + className+ " error: " + msg);
         }
 
         public void info(String msg) {
-            System.out.println(LocalDateTime.now() + " " + className + " info: " + msg);
+            printStream.println(LocalDateTime.now() + " " + className + " info: " + msg);
         }
 
 
         public void debug(String msg) {
             if (Debug.isEnable()) {
-                System.out.println(LocalDateTime.now()  + " " + className + " debug: " + msg);
+                printStream.println(LocalDateTime.now()  + " " + className + " debug: " + msg);
             }
         }
-
-
     }
-
 
     private static class Debug {
         private static Boolean enable = null;
