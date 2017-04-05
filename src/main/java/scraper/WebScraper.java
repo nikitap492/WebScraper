dae5cd94a7a29e9d6b88897c4ccd3f1a85efdda6
Nik <nikitap4.92@gmail.com>
2017-04-05 11:59:44 +0300

    New output

--- src/main/java/scraper/WebScraper.java
+++ src/main/java/scraper/WebScraper.java
@@ -1,9 +1,19 @@
 package scraper;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scraper.config.Configuration;
+import scraper.domain.ConsoleParseData;
+import scraper.domain.EngineAnalyze;
 import scraper.engine.impl.EngineConfigurator;
-import scraper.logger.Logger;
-import scraper.logger.LoggerFactory;
+import scraper.parser.ConsoleParser;
+import scraper.parser.impl.DefaultConsoleParser;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Poshivalov Nikita
@@ -15,7 +25,6 @@ import scraper.logger.LoggerFactory;
  * 1) URL or path to file with urls
  * 2) word or words separated by delimeter ","
  * 3) Optional options:
- *              -v is verbose (enables debug)
  *              -e is analyze and print sentences in which were found word from 2)
  *              -w is word analyze (counter for each word from 2) on all sources)
  *              -c is char analyze (counts chars in web pages overall)
@@ -23,7 +32,7 @@ import scraper.logger.LoggerFactory;
  */
 public class WebScraper {
 
-    private static final Logger log = LoggerFactory.obtain(WebScraper.class);
+    private static final Logger log = LoggerFactory.getLogger(WebScraper.class);
 
     /**
      * Main class of application
@@ -37,9 +46,29 @@ public class WebScraper {
      */
     public static void main(String args[]) {
         log.info("Application starting ....");
-        EngineConfigurator
-                .configure()
-                .defaultConfiguration()
-                .run(args);
+
+        // Console parse
+
+        ConsoleParser parser = new DefaultConsoleParser();
+        ConsoleParseData consoleParseData = parser.parse(Arrays.stream(args)
+                .collect(Collectors.toList()));
+
+        Configuration configuration = consoleParseData.getConfiguration();
+        List<URL> sources = consoleParseData.getUrls();
+        List<String> words = consoleParseData.getWords();
+
+        // Run engine
+        EngineAnalyze analyze = EngineConfigurator
+                .configure(configuration)
+                .search(words)
+                .useDefaultComponents()
+                .run(sources);
+
+        //simple output
+        analyze.words().forEach((word, counter) -> log.info("Word : " + word + " was found in sources " + counter + " times"));
+        analyze.sentences()
+                .forEach((word, sentences) -> log.info("Word : " + word + " was found in sentences ' " + sentences + " '"));
+        log.info("Sum of chars on web pages equals " + analyze.chars());
     }
+
 }
